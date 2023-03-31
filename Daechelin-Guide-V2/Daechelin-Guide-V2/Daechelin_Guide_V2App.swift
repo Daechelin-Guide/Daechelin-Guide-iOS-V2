f147dd27f6c65b8de6d4dcfff7454335ce311647
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-03-31 19:02:48 +0900

    프로젝트 커밋
    프로젝트 커밋

---
+++ Daechelin-Guide-V2/Daechelin-Guide-V2/Daechelin_Guide_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Daechelin_Guide_V2App.swift
+//  Daechelin-Guide-V2
+//
+//  Created by 이민규 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct Daechelin_Guide_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
