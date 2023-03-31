2cbe2f35cab7ee2284ef9e3e417487778dc7c3fe
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-04-01 00:12:33 +0900

    xcode 오류 해결
    cosmos 관련오류 해결

---
+++ Daechelin-Guide-iOS-V2/Daechelin-Guide-iOS-V2/Daechelin_Guide_iOS_V2App.swift
@@ -0,0 +1,17 @@
+//
+//  Daechelin_Guide_iOS_V2App.swift
+//  Daechelin-Guide-iOS-V2
+//
+//  Created by 이민규 on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct Daechelin_Guide_iOS_V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
